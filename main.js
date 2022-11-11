e6002baa28e51738cdc32805502507f8cba318ba
cwest9400 <chrstphrwest@gmail.com>
2022-11-11 14:44:25 -0500

    Decrement function working for hunger stat

--- main.js
+++ main.js
@@ -5,28 +5,44 @@ const sleepyStat = document.querySelector('.sleepyStat')
 const boredomStat = document.querySelector('.boredomStat')
 //start the game(timer button)
 const startTimer = document.querySelector('.startButton')
+//decrement buttons
+const feedButton = document.querySelector(".feedButton")
+const lightsButton = document.querySelector(".lightsButton")
+const playButton = document.querySelector(".playButton")
+
 
 //timer code
+let intervalHungerStat
 
-let intervalStat
-///might have to fix this later
+///might have to fix this later(hungerStat name)
 let count = Number(hungerStat.textContent)
-
-
-
 
 
 /////timer functions
 
 function updateStat(num) {
     hungerStat.textContent = num
-    sleepyStat.textContent = num
-    boredomStat.textContent = num
+    // sleepyStat.textContent = num
+    // boredomStat.textContent = num
     if(num == 10) {
-        clearInterval(intervalStat)
+        clearInterval(intervalHungerStat)
       } 
 }
-////decrement function
+
+////decrement functions
+//decrement hunger
+function decrementHunger () {
+    if(count > 0) {
+        count --
+        hungerStat.textContent = count
+    } 
+}
+
+
+
+    
+
+
 
 
 
@@ -34,12 +50,15 @@ function updateStat(num) {
 
 
 //eventlisteners
+//timers
+
 startTimer.addEventListener("click", function () {
     intervalStat = setInterval(() => {
-      count += 1;
-      updateStat(count);      
-     }, 1000);
-    startTimer.disabled = true;
-  });
+      count += 1
+      updateStat(count)     
+     }, 1000)
+    startTimer.disabled = true
+  })
   
-  
+ //decrement listners
+feedButton.addEventListener('click', decrementHunger)
