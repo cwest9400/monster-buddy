8b4fe593a0d78be00035bc80b0923916f714f3f8
cwest9400 <chrstphrwest@gmail.com>
2022-11-11 15:40:28 -0500

    sleep timer + decrementer + format cleanup

--- main.js
+++ main.js
@@ -1,43 +1,85 @@
 //targets
-//update display number(1-10) based on timer 
+///update display number(1-10) based on timer 
 const hungerStat = document.querySelector('.hungerStat')
 const sleepyStat = document.querySelector('.sleepyStat')
 const boredomStat = document.querySelector('.boredomStat')
-//start the game(timer button)
+///start the game(timer button)
 const startTimer = document.querySelector('.startButton')
-//decrement buttons
+///decrement buttons
 const feedButton = document.querySelector(".feedButton")
 const lightsButton = document.querySelector(".lightsButton")
 const playButton = document.querySelector(".playButton")
 
 
-//timer code
+//timer intervals
 let intervalHungerStat
+let intervalSleepyStat
+let intervalBoredomStat
 
-///might have to fix this later(hungerStat name)
-let count = Number(hungerStat.textContent)
+//stat counts
+let countHunger = Number(hungerStat.textContent)
+let countSleepiness = Number(sleepyStat.textContent)
+let countBoredom = Number(boredomStat.textContent)
 
 
-/////timer functions
-
-function updateStat(num) {
+//timer functions
+///hunger function
+function updateHungerStat(num) {
     hungerStat.textContent = num
-    // sleepyStat.textContent = num
-    // boredomStat.textContent = num
+    
     if(num == 10) {
         clearInterval(intervalHungerStat)
-      } 
+        clearInterval(intervalSleepyStat)
+        clearInterval(intervalBoredomStat)
+        feedButton.disabled = true
+        lightsButton.disabled = true
+        playButton.disabled = true
+        hungerStat.textContent = "OH NO!! Your pet doesn't look so great!"
+    } 
+        
+    
 }
+///sleepy function
+function updateSleepyStat(num2) {
+    sleepyStat.textContent = num2
+    
+    if(num2 == 10) {
+        clearInterval(intervalHungerStat)
+        clearInterval(intervalSleepyStat)
+        clearInterval(intervalBoredomStat)
+        feedButton.disabled = true
+        lightsButton.disabled = true
+        playButton.disabled = true
+        hungerStat.textContent = "OH NO!! Your pet doesn't look so great!"
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
 
 ////decrement functions
 //decrement hunger
 function decrementHunger () {
-    if(count > 0) {
-        count --
-        hungerStat.textContent = count
-    } 
+    if(countHunger > 0) {
+        countHunger --
+        hungerStat.textContent = countHunger  
 }
-
+}
+//decrement sleepiness
+function decrementSleepiness () {
+    if(countSleepiness > 0) {
+        countSleepiness --
+        sleepyStat.textContent = countSleepiness  
+}
+}
 
 
     
@@ -51,14 +93,23 @@ function decrementHunger () {
 
 //eventlisteners
 //timers
-
+///Hunger timer
 startTimer.addEventListener("click", function () {
-    intervalStat = setInterval(() => {
-      count += 1
-      updateStat(count)     
-     }, 1000)
+    intervalHungerStat = setInterval(() => {
+      countHunger += 1
+      updateHungerStat(countHunger)     
+    }, 1000)
+    intervalSleepyStat = setInterval(() => {
+        countSleepiness += 1
+        updateSleepyStat(countSleepiness)     
+    }, 3000)
     startTimer.disabled = true
   })
-  
- //decrement listners
+
+
+ 
+ 
+ 
+  //decrement listners
 feedButton.addEventListener('click', decrementHunger)
+lightsButton.addEventListener('click', decrementSleepiness)
